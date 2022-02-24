425f91cb7229f2b1cf02109fd868dc2938d4f87a
nulldev <contact@andybao.me>
2022-02-24 00:16:47 -0500

    Add Flatpak installation instructions

--- js/setup/setup.js
+++ js/setup/setup.js
@@ -1,4 +1,8 @@
 const distDropdown = document.getElementById("distribution_select");
+const distDropdownWrapper = document.getElementById("distribution_select_wrapper");
+const linuxFlatpakSelectWrapper = document.getElementById("linux_flatpak_select_wrapper");
+const linuxFlatpakArchSelectWrapper = document.getElementById("linux_flatpak_arch_select_wrapper");
+const archList = ["x86-64", "x86-32", "arm", "other"];
 async function init() {
     const platformInfo = await browser.runtime.getPlatformInfo(); 
     const browserInfo = await browser.runtime.getBrowserInfo();
@@ -6,13 +10,27 @@ async function init() {
     const browserName = browserInfo.name; // Firefox
     const platform = platformInfo.os; // mac, win, android, cros, linux, openbsd
     let arch = platformInfo.arch; // x86-64, x86-32, arm
-    if(arch !== "x86-64" && arch !== "x86-32" && arch !== "arm")
+    if(!archList.includes(arch))
         arch = "other";
 
-    postInit(browserName, platform, arch);
+    linuxFlatpakArchSelectWrapper.onchange = e => {
+        if(e.target.name === 'linux_flatpak_arch') {
+            for(const element of document.querySelectorAll(".content > .instructions .flatpak-arch")) {
+                element.classList.add("hidden");
+            }
+            for(const element of document.querySelectorAll(".content > .instructions .flatpak-arch." + e.target.value)) {
+                element.classList.remove("hidden");
+            }
+        }
+    };
+    const flatpakArchElement = document.getElementById("linux_flatpak_arch_" + arch);
+    flatpakArchElement.checked = true;
+    flatpakArchElement.dispatchEvent(new Event('change', { bubbles: true })); // Trigger onchange
+
+    postInit(browserName, platform, arch, null);
 }
 
-function postInit(browserName, platform, arch) {
+function postInit(browserName, platform, arch, flatpak) {
     // TODO Handle different browser
     
     const advanced = window.location.hash === "#advanced";
@@ -44,7 +62,7 @@ function postInit(browserName, platform, arch) {
     }
 
     // Update arch instructions
-    for(const potentialArch of ["x86-64", "x86-32", "arm", "other"]) {
+    for(const potentialArch of archList) {
         for(const element of document.querySelectorAll(".content > .instructions .arch." + potentialArch)) {
             if((advanced && !element.classList.contains("unsupported")) || (!advanced && element.classList.contains(arch))) {
                 element.classList.remove("hidden");
@@ -78,19 +96,33 @@ function postInit(browserName, platform, arch) {
         }
     }
 
-    window.onhashchange = () => postInit(browserName, platform, arch);
-    distDropdown.onchange = updateLinuxDist;
-    updateLinuxDist();
+    linuxFlatpakSelectWrapper.onchange = e => {
+        if(e.target.name === 'linux_flatpak') {
+            postInit(browserName, platform, arch, e.target.value === "yes");
+        }
+    };
+
+    const showLinuxDist = flatpak === false;
+    distDropdownWrapper.classList[showLinuxDist ? "remove" : "add"]("hidden");
+    for(const element of document.querySelectorAll(".content > .instructions .flatpak")) {
+        element.classList[flatpak === true ? "remove" : "add"]("hidden");
+    }
+
+    window.onhashchange = () => postInit(browserName, platform, arch, flatpak);
+    distDropdown.onchange = () => updateLinuxDist(showLinuxDist);
+    updateLinuxDist(showLinuxDist);
 }
 
-function updateLinuxDist() {
+function updateLinuxDist(show) {
     const newVal = distDropdown.value;
 
     for(const dist of document.querySelectorAll(".content > .instructions.linux .distribution")) {
         dist.classList.add("hidden");
     }
-    for(const dist of document.querySelectorAll(".content > .instructions.linux .distribution." + newVal)) {
-        dist.classList.remove("hidden");
+    if(show) {
+        for(const dist of document.querySelectorAll(".content > .instructions.linux .distribution." + newVal)) {
+            dist.classList.remove("hidden");
+        }
     }
 }
 
