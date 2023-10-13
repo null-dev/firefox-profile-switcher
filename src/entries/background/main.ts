9aa59b31f3d91aa529cc5a4cb63ff1cc640f1258
retiolus <retiolus@tuta.io>
2023-10-13 13:32:16 +0200

    implement context menu switcher

--- src/entries/background/main.ts
+++ src/entries/background/main.ts
@@ -1,6 +1,7 @@
 import browser from "webextension-polyfill";
 import type {Action} from "webextension-polyfill";
 import {initNative} from "~/lib/native";
+import {nativeLaunchProfile} from  "~/lib/native";
 import {
   EXTENSION_ID,
   fetchAndRoundAvatarAsCanvas, REQUEST_TYPE_CLOSE_MANAGER, STORAGE_CACHE_CUSTOM_AVATARS, STORAGE_CACHE_GLOBAL_OPTIONS,
@@ -56,6 +57,65 @@ async function handleEvent(event) {
     browser.storage.local.set({[STORAGE_CACHE_PROFILE_ORDER]: event.order});
   }
 }
+
+// Function to handle a specific profile selection from the context menu
+async function handleProfileSelection(info, tab) {
+  console.log("Profile selected from context menu.");
+  console.log(info);
+  console.log(tab);
+
+  const selectedProfile = info.menuItemId.split('-')[1]; // Extract profile name from the ID
+
+  const urlToOpen = info.pageUrl;
+  console.log(`URL to open with profile: ${urlToOpen}`);
+
+  // Fetch the list of profiles from storage
+  const profileListData = await browser.storage.local.get(STORAGE_CACHE_PROFILE_LIST);
+
+  if (profileListData && profileListData[STORAGE_CACHE_PROFILE_LIST]) {
+      // Find the profile with the name entered by the user
+      const desiredProfile = profileListData[STORAGE_CACHE_PROFILE_LIST].profiles.find(profile => profile.name === selectedProfile);
+
+      if (desiredProfile) {
+          // Use the ID of the selected profile to call nativeLaunchProfile
+          await nativeLaunchProfile(desiredProfile.id, urlToOpen);
+          console.log(`Profile "${selectedProfile}" launched successfully with URL: ${urlToOpen}`);
+      } else {
+          console.error(`Profile "${selectedProfile}" not found!`);
+      }
+  } else {
+      console.error('Failed to retrieve profiles from storage!');
+  }
+}
+
+// Create a context menu
+browser.contextMenus.create({
+  id: "switch-profile",
+  title: "Open with another profile...",
+  contexts: ["page"]
+});
+
+// Add profiles to the context menu
+async function addProfilesToContextMenu() {
+  const profileListData = await browser.storage.local.get(STORAGE_CACHE_PROFILE_LIST);
+
+  if (profileListData && profileListData[STORAGE_CACHE_PROFILE_LIST]) {
+      for (const profile of profileListData[STORAGE_CACHE_PROFILE_LIST].profiles) {
+          browser.contextMenus.create({
+              id: `profile-${profile.name}`,
+              title: profile.name,
+              parentId: "switch-profile",
+              contexts: ["page"],
+              onclick: handleProfileSelection
+          });
+      }
+  } else {
+      console.error('Failed to retrieve profiles from storage!');
+  }
+}
+
+// Call the function to add profiles to the context menu
+addProfilesToContextMenu();
 
 initNative(handleEvent);
 
